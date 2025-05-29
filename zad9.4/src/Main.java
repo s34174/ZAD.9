1399f53012656f0a615085518c853b766d8969b2
s34174 <s34174@pjwstk.edu.pl>
2025-05-29 10:55:48 +0300

    rozwiazanie4

---
+++ zad9.4/src/Main.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        fillArray(array);
+
+        System.out.println("Zawartość tablicy:");
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+    }
+
+    // Метод, який заповнює масив з обробкою винятку
+    public static void fillArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            try {
+                array[i] = readNumber();
+            } catch (NegativeNumberException e) {
+                System.out.println(e.getMessage());
+                array[i] = 0;
+            }
+        }
+    }
+
+    // Метод, який читає число з клавіатури
+    public static int readNumber() throws NegativeNumberException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Wprowadź liczbę: ");
+        int number = scanner.nextInt();
+        if (number < 0) {
+            throw new NegativeNumberException("Negative numbers are not allowed");
+        }
+        return number;
+    }
+}
+
+// Клас власного винятку
+class NegativeNumberException extends Exception {
+    public NegativeNumberException(String message) {
+        super(message);
+    }
+}
